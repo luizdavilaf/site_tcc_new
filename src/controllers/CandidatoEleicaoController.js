b0a30388fc90426752339a192c527b539ac95392
Luiz D Avila <85142503+luizdavilaf@users.noreply.github.com>
2023-10-16 01:08:57 -0300

    adicionando graficos

--- src/controllers/CandidatoEleicaoController.js
+++ src/controllers/CandidatoEleicaoController.js
@@ -19,7 +19,7 @@ const getFormData = async (req, res) => {
         if (!req.query.cargo) {
             throw new Error("cargo deve ser selecionado")
         }
-        const response = await Promise.all([
+        const data = await Promise.all([
             CandidatoEleicaoService.getCandidatoEleicaoByDegree(parseInt(req.query.eleicao), parseInt(req.query.regiao), req.query.situacao_turno, req.query.cargo),
             CandidatoEleicaoService.getCandidatoEleicaoByGender(parseInt(req.query.eleicao), parseInt(req.query.regiao), req.query.situacao_turno, req.query.cargo),
             CandidatoEleicaoService.getCandidatoEleicaoByAge(parseInt(req.query.eleicao), parseInt(req.query.regiao), req.query.situacao_turno, req.query.cargo),
@@ -27,11 +27,60 @@ const getFormData = async (req, res) => {
             CandidatoEleicaoService.getCandidatoEleicaoByParty(parseInt(req.query.eleicao), parseInt(req.query.regiao), req.query.situacao_turno, req.query.cargo),
             CandidatoEleicaoService.getCandidatoEleicaoByRace(parseInt(req.query.eleicao), parseInt(req.query.regiao), req.query.situacao_turno, req.query.cargo),
             CandidatoEleicaoService.getCandidatoEleicaoReelection(parseInt(req.query.eleicao), parseInt(req.query.regiao), req.query.situacao_turno, req.query.cargo),
-
         ])
+        //console.log(data)
 
+/* const labelstext<%= index %> = <%= (category.labels) %> ;
+            const labels<%= index %> = labelstext<%= index %>.map(label => unescape(label)); */
 
-        console.log(response)
+        const chartData = [
+            {
+                id:"graudeinstrucao",
+                categoria: "Grau de Instrução",
+                labels: data[0].map((item) => item.grau_instrucao),
+                data: data[0].map(item => item.totalCandidatos)
+            },
+            {
+                id: "genero",
+                categoria: "Gênero",
+                labels: data[1].map(item => item.genero),
+                data: data[1].map(item => item.totalCandidatos)
+            },
+            {
+                id: "idade",
+                categoria: "Idade",
+                labels: data[2].map(item => item.idade),
+                data: data[2].map(item => item.totalCandidatos)
+            },
+            {
+                id: "ocupacao",
+                categoria: "Ocupação",
+                labels: data[3].map(item => item.ocupacao),
+                data: data[3].map(item => item.totalCandidatos)
+            },
+            {
+                id: "partido",
+                categoria: "Partido",
+                labels: data[4].map(item => item.partido),
+                data: data[4].map(item => item.totalCandidatos)
+            },
+            {
+                id: "raca",
+                categoria: "Raça",
+                labels: data[5].map(item => item.raca ? item.raca : "Sem dados"),
+                data: data[5].map(item => item.totalCandidatos)
+            },
+            {
+                id: "reeleito",
+                categoria: "Reeleito",
+                labels: data[6].map(item => item.reeleito ? "SIM" : "NAO"),
+                data: data[6].map(item => item.totalCandidatos)
+            }
+        ]
+        console.log(data[6])
+        //console.log({ grau_de_instrucao: JSON.stringify(grau_de_instrucao), genero: JSON.stringify(genero), idade, ocupacao, partido, raca })
+
+        res.render('results-year-charts.ejs', { chartData });
 
     } catch (error) {
         console.log(error)
