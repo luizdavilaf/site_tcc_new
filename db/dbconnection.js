0eb8000b570cf8f26216174c4ef48dcb3442521a
Luiz D Avila <85142503+luizdavilaf@users.noreply.github.com>
2023-12-15 13:47:05 -0300

    ajuste

--- db/dbconnection.js
+++ db/dbconnection.js
@@ -8,6 +8,12 @@ module.exports = {
         host: config.urldb,
         dialect: 'postgres',
         logging: false,
+        pool: {
+            max: 5, // número máximo de conexões no pool
+            min: 0, // número mínimo de conexões no pool
+            acquire: 30000, // tempo máximo, em milissegundos, que a pool tentará obter uma conexão antes de lançar um erro
+            idle: 10000 // tempo máximo, em milissegundos, que uma conexão pode ficar inativa no pool antes de ser desconectada
+        }
     },
     
     // Adicione configurações para produção e teste, se necessário.
